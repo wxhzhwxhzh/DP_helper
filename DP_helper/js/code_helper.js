f1e97c8cf6f8c48d9c9bfec8317aa846e14ff500
wxhzhwxhzh <you@example.com>
2024-07-22 14:02:02 +0800

    简化  启动代码生成！！

--- DP_helper/js/code_helper.js
+++ DP_helper/js/code_helper.js
@@ -9,13 +9,6 @@ var code_frame =`
 #-导入库
 from DrissionPage import ChromiumPage,ChromiumOptions
 
-
-#-配置类
-class Config:    
-    UA_android="Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.62 Mobile Safari/537.36"
-    UA_apple="Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1"
-
-
 #-创建配置对象
 co=ChromiumOptions()
 
@@ -27,9 +20,6 @@ co=ChromiumOptions()
 
 #-创建浏览器
 page = ChromiumPage(addr_or_opts=co)
-
-#-设置文件下载目录 默认是当前目录
-page.set.download_path(".")
 
 #cookie
 #-打开网址
